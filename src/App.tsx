b61d6c945c177a10406324f0ad43c78756b071e0
WeStride <fullstack.we.stride@gmail.com>
2023-05-18 21:59:23 +0800

    Added bot card component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import BotAppNavbar from "./components/BotAppNavbar";
+import BotCard from "./components/BotCard";
 
 function App() {
   return (
     <div className="App">
       <BotAppNavbar />
-      <Container></Container>
+      <Container className="d-flex justify-content-center my-5">
+        <BotCard />
+      </Container>
     </div>
   );
 }
---
+++ src/components/BotCard.tsx
@@ -0,0 +1,178 @@
+import React, { useState } from "react";
+
+// React Spring
+import { useSpring, animated } from "@react-spring/web";
+
+// Mui Components
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+
+// Icons
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital"; // health
+import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment"; // attack
+import ShieldIcon from "@mui/icons-material/Shield"; // defense
+import PsychologyIcon from "@mui/icons-material/Psychology"; // intelligence
+import BatteryChargingFullIcon from "@mui/icons-material/BatteryChargingFull"; // energy
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { mockBot } from "../mock/mockData";
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+const BotStatContainer = (props: any) => {
+  const { statName, statValue, IconComponent } = props;
+  return (
+    <div className="d-flex justify-content-between mx-5 my-1">
+      <div className="d-flex">
+        {IconComponent}
+        <Typography style={{ fontWeight: "bold" }}>{statName}:</Typography>
+      </div>
+      <Typography style={{ fontWeight: "bold" }}>{statValue}</Typography>
+    </div>
+  );
+};
+
+const BotCard = () => {
+  const [expanded, setExpanded] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const data = mockBot;
+  const {
+    id,
+    name,
+    purpose,
+    attack,
+    defense,
+    health,
+    energy,
+    intelligence,
+    picture,
+    isRare,
+    isFavorite,
+  } = data;
+
+  const [springs, api] = useSpring(() => ({
+    from: { display: "none" },
+  }));
+
+  const handleOnMouseEnter = () => {
+    api.start({
+      from: { display: "none" },
+      to: { display: "flex" },
+      reset: true,
+    });
+  };
+
+  const handleOnMouseLeave = () => {
+    api.start({
+      from: { display: "flex" },
+      to: { display: "none" },
+    });
+  };
+
+  const RenderBotStats = () => {
+    return (
+      <animated.div
+        className="flex-column w-100 h-100 justify-content-center"
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "0",
+          backgroundColor: "rgba(255, 255, 255, 0.3)",
+          ...springs,
+        }}
+      >
+        <BotStatContainer
+          statName={"Health"}
+          statValue={health}
+          IconComponent={<LocalHospitalIcon />}
+        />
+        <BotStatContainer
+          statName={"Attack"}
+          statValue={attack}
+          IconComponent={<LocalFireDepartmentIcon />}
+        />
+        <BotStatContainer
+          statName={"Defense"}
+          statValue={defense}
+          IconComponent={<ShieldIcon />}
+        />
+        <BotStatContainer
+          statName={"Intelligence"}
+          statValue={intelligence}
+          IconComponent={<PsychologyIcon />}
+        />
+        <BotStatContainer
+          statName={"Energy"}
+          statValue={energy}
+          IconComponent={<BatteryChargingFullIcon />}
+        />
+      </animated.div>
+    );
+  };
+
+  const pictureUrl = `data:image/svg+xml,${encodeURIComponent(picture)}`;
+  return (
+    <Card
+      sx={{ maxWidth: 345 }}
+      style={{
+        backgroundColor: "#D4ADFC",
+        border: "10px solid #5C469C",
+        position: "relative",
+      }}
+      onMouseEnter={() => handleOnMouseEnter()}
+      onMouseLeave={() => handleOnMouseLeave()}
+    >
+      <CardHeader
+        avatar={<Avatar sx={{ bgcolor: "#1D267D" }}>#{id}</Avatar>}
+        action={
+          <IconButton aria-label="add to favorites" style={{ zIndex: 1 }}>
+            <FavoriteIcon />
+          </IconButton>
+        }
+        title={name}
+        titleTypographyProps={{
+          fontSize: 25,
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap",
+          fontWeight: "bold",
+        }}
+        color="#1D267D"
+      />
+      {RenderBotStats()}
+      <CardMedia component="img" src={pictureUrl} />
+      <CardContent>
+        <Typography variant="body2" color="#1D267D" fontSize={18}>
+          {purpose}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default BotCard;
